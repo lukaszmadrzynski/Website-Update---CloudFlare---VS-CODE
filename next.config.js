e041721170125e04009e5132a6a79234093d8496
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-03-24 12:52:16 +0800

    Add static export configuration to Next.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     env: {
         stackbitPreview: process.env.STACKBIT_PREVIEW
     },
+    output: 'export',
     trailingSlash: true,
     reactStrictMode: true,
     distDir: 'out'
