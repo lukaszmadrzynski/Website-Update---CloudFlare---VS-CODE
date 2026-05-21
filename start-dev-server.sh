f499cde32fd3cec9451a117e3755774b3935068a
lukaszmadrzynski <lukasz.madrzynski@yahoo.com>
2026-05-21 13:51:52 +0800

    Add dev server startup scripts for Windows (.bat) and macOS/Linux (.sh)

---
+++ start-dev-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Start Cloud Mountain Ecotour website on localhost:3000
+
+echo "Starting Cloud Mountain Ecotour website..."
+echo "Open http://localhost:3000 in your browser"
+echo "Press Ctrl+C to stop the server"
+echo ""
+
+npm run dev
